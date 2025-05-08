2ecece9fc9d6e14d03f15dc5047b0197e7df9f9f
alphonse tugirimana <tugirimanaalphonse@gmail.com>
2025-05-08 06:29:23 -0700

    on other commit three

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,42 +1,15 @@
-<!-- resources/views/products/index.blade.php -->
+<!-- resources/views/products/show.blade.php -->
 @extends('layouts.app1')
 
 @section('content')
 <div class="container">
-    <h1>Product List</h1>
-    <a href="{{ route('products.create') }}" class="btn btn-primary mb-3">Add New Product</a>
+    <h1>Product Details</h1>
     
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+    <div class="form-group">
+        <label for="PName">Product Name</label>
+        <input type="text" class="form-control" value="{{ $product->PName }}" disabled>
+    </div>
 
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Product Name</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($products as $product)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $product->PName }}</td>
-                    <td>
-                        <a href="{{ route('products.show', $product->PCode) }}" class="btn btn-info">View</a>
-                        <a href="{{ route('products.edit', $product->PCode) }}" class="btn btn-warning">Edit</a>
-                        <form action="{{ route('products.destroy', $product->PCode) }}" method="POST" style="display:inline-block;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <a href="{{ route('products.index') }}" class="btn btn-secondary mt-3">Back to Product List</a>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,8 @@
-e>Stock Management System</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Stock Management System</title>
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
